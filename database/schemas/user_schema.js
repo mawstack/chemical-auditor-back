e5e81f23142f8854bc91bc83106919200a8ceaa6
maxPiroddi <mpiroddi@outlook.com>
2020-01-21 14:17:42 +1100

    Quick styling changes

--- database/schemas/user_schema.js
+++ database/schemas/user_schema.js
@@ -2,22 +2,22 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    username: {
-        type: String,
-        required: true
-    },
-    isAdmin: {
-        type: Boolean,
-        required: true
-    }
-})
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  isAdmin: {
+    type: Boolean,
+    required: true
+  }
+});
 
 module.exports = UserSchema;
