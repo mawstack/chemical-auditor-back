444bcbd562739e5de3ea61b6f8c4f90804faac1d
poklipart <angellopezandrade4@gmail.com>
2020-01-21 13:14:57 +1100

    user CUD API routes - tentative (untested)

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,1 +1,37 @@
 const UserModel = require("./../database/models/user_model");
+
+//GET /register
+function newUser (req, res) {
+    //display the new User React view
+}
+
+//POST /register
+async function create (req, res) {
+    //create user in db using data from form-post request body
+    //then redirect to dashboard AND pass token to login
+
+    //!!Add return test for postman - 21/01 1:14PM
+}
+
+//GET /users/:id/edit
+function edit (req, res) {
+    //display the edit User React view
+}
+
+//PUT /users/:id
+async function update (req, res) {
+    //change the specified user in the DB then redirect to dashboard
+}
+
+//DELETE users/:id
+async function deleteUser (req, res) {
+    //delete the specified user from the DB then redirect to dashboard
+}
+
+module.exports = {
+    newUser,
+    create,
+    edit,
+    update,
+    deleteUser
+}
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const userRoutes = require("./user_routes");
+
+
+
+app.use("/users", userRoutes);
+module.exports = router;
+
+
+
+
 /* Chemical Auditor API - Routes reference
 - CUD Users
 - CRUD Entries
 - RU Reports(?)
 
 Users
-GET /login - Login (view)
 GET /register - New user (view)
 POST /register - Create user
-GET /user/:id - Edit user (view)
+GET /users/:id/edit - Edit user (view)
 PUT /users/:id - Update user
 DELETE /users/:id - Destroy user
 
@@ -20,7 +31,8 @@ DELETE /entries/:id - Destroy entry
 GET /entries/:id/edit - Edit entry (view)
 PUT+PATCH /entries/:id - Update entry
 
-Other
+Pages (Other Routes)
+GET /login - Login (view)
 GET / - dashboard (view)
 
 Notes
@@ -31,4 +43,3 @@ Notes
 - Report left for later - 21/01
 
 */
-
