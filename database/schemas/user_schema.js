4224cc7f86f483921b6aae12c1a49153aa02f5c4
maxPiroddi <mpiroddi@outlook.com>
2020-01-21 12:50:08 +1100

    Basic mongo setup

--- database/schemas/user_schema.js
+++ database/schemas/user_schema.js
@@ -2,8 +2,20 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
     username: {
         type: String,
+        required: true
+    },
+    isAdmin: {
+        type: Boolean,
         required: true
     }
 })
