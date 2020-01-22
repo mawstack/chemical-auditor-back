1cf13b6763257d016a35ccf6dd7bb74a09fc2b6b
poklipart <angellopezandrade4@gmail.com>
2020-01-22 15:09:45 +1100

    celebrate post/put request body validation completed

--- routes/entry_routes.js
+++ routes/entry_routes.js
@@ -1,13 +1,46 @@
 const express = require("express");
 const router = express.Router();
 const EntryController = require("./../controllers/entry_controller");
+const { celebrate, Joi, Segments } = require("celebrate");
 
 router.get("/", EntryController.index);
 router.get("/new", EntryController.newEntry);
-router.post("/", EntryController.create);
+router.post("/", celebrate({
+    [Segments.BODY]: {
+        startTime: Joi.number().required(),
+        finishTime: Joi.number().required(),
+        currentLat: Joi.number().required(),
+        currentLong: Joi.number().required(),
+        cropRow: Joi.number().required(),
+        chemicalUsed: Joi.string().required(),
+        WHP: Joi.number().required(),
+        EHD: Joi.string().required(),
+        rateApplied: Joi.number().required(),
+        quantityApplied: Joi.number().required(),
+        equipmentMethodUsed: Joi.string().required(),
+        speed: Joi.number().required(),
+        deg: Joi.number().required()
+    }
+}), EntryController.create);
 router.get("/:id", EntryController.show);
 router.delete("/:id", EntryController.deleteEntry);
 router.get("/:id/edit", EntryController.edit);
-router.put("/:id", EntryController.update);
+router.put("/:id", celebrate({
+    [Segments.BODY]: {
+        startTime: Joi.number().required(),
+        finishTime: Joi.number().required(),
+        currentLat: Joi.number().required(),
+        currentLong: Joi.number().required(),
+        cropRow: Joi.number().required(),
+        chemicalUsed: Joi.string().required(),
+        WHP: Joi.number().required(),
+        EHD: Joi.string().required(),
+        rateApplied: Joi.number().required(),
+        quantityApplied: Joi.number().required(),
+        equipmentMethodUsed: Joi.string().required(),
+        speed: Joi.number().required(),
+        deg: Joi.number().required()
+    }
+}), EntryController.update);
 
 module.exports = router;
