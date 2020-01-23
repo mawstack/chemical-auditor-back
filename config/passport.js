949e8c29b06a7baabecdca1a11f7fdc355b52fd3
poklipart <angellopezandrade4@gmail.com>
2020-01-23 13:23:42 +1100

    untested jwt payload logic in config/passport

--- config/passport.js
+++ config/passport.js
@@ -1,8 +1,10 @@
 const passport = require("passport");
-
-//Local Strategy for UserModel email + password authentication
 const LocalStrategy = require("passport-local");
 const UserModel = require("../database/models/user_model");
+const { Strategy: JwtStrategy, ExtractJwt } = require("passport-jwt");
+
+
+//Local Strategy for UserModel email + password login authentication
 
 //'Serialization' - writing data as a string (here for storage in internal passport tracker)
 //Receive a user object (i.e. the current user) from the DB >
@@ -25,20 +27,58 @@ passport.deserializeUser(async (id, done) => {
     }
 });
 
-//Declaring + Setting a new Local Strategy
+//Declaring + passport-setting a new LocalStrategy
 //First setting the passport named-argument 'usernameField' to 'email' (declaring that 'email' in the DB will function as the user's name)
 //async function declares the actual local strategy logic, here - receiving an 'name' (email) and password...
 //...finding a matching-email user in the DB + checking password and returning authentication failure/success...
 //...done-chaining with false = failure, done-chaining with retrieved user record = success
-passport.use(new LocalStrategy({
-    usernameField: "email"
-    },
-    async (email, password, done) => {
-        const user = await UserModel.findOne({ email })
-            .catch(done);
-        if (!user || !user.verifyPasswordSync(password)) {
-            return done(null, false);
+passport.use(
+    new LocalStrategy({
+        usernameField: "email"
+        },
+        async (email, password, done) => {
+            const user = await UserModel.findOne({ email })
+                .catch(done);
+            if (!user || !user.verifyPasswordSync(password)) {
+                return done(null, false);
+            }
+            return done(null, user);
         }
-        return done(null, user);
-    }
+    )
+);
+
+//
+
+//JWT Strategy for authentication token stored in request header (Note: should be encrypted when stored)
+//JwtStrategy logic consisting of an object (with a 'get token' method declaration and the jwt key)...
+//...and an async function that retrieves the user matching that of the jwt requests payload subject
+passport.use(
+    new JwtStrategy(
+        {
+            getJwtTokenFromReq: (req) => {
+                let token = null;
+                //previous version - differences?
+                /*
+                if(req.session && req.session.jwt) {
+                    return req.session.jwt;
+                } else {
+                    return null;
+                }
+                */
+                if (req && req.cookies) {
+                    token = req.cookies("jwtToken");
+                }
+                return token;
+            },
+            jwt_key: process.env.JWT_KEY
+        },
+        async(jwtPayload, done) => {
+            const user = await UserModel.findById(jwtPayload.sub)
+                .catch(done);
+            if(!user) {
+                return done(null, false);
+            }
+            return done(null, user);
+        }
+    )
 );
